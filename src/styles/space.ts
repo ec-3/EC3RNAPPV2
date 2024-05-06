5433750b9dfc01f8252befd8d311b7b0bb740956
Aaron <aaronshihero@gmail.com>
2024-05-06 16:17:27 +0800

    增加存储和样式相关内容

---
+++ src/styles/space.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const grid = 16;
+
+export const SpaceStyle = StyleSheet.create({
+  container: {
+    paddingLeft: grid / 2,
+    paddingRight: grid / 2,
+  },
+
+  oneContainer: {
+    paddingLeft: grid,
+    paddingRight: grid,
+    // backgroundColor:'red'
+  },
+});
